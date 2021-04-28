ad6f4388d454b072ab671842ba61e59bcf11b2d7
ForgottenYouth <540658894@qq.com>
2021-04-28 15:58:27 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CPlugProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlugProject.dir/T7_Demo.cpp.obj"
+  "CPlugProject.exe"
+  "CPlugProject.exe.manifest"
+  "CPlugProject.pdb"
+  "libCPlugProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlugProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
